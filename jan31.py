cd86b6c58c0e79575a704998000e6493f3390d69
Jack Willis <jack.willis@worldcovr.com>
2019-01-31 16:46:40 -0500

    Updated Jan31
    Jan31

--- jan31.py
+++ jan31.py
@@ -1,4 +1,10 @@
-import numpy
-import scipy
-import pandas
+import numpy as np
+import scipy as sp
+import pandas as pd
 
+n = 100
+
+t=pd.date_range('20190101',periods=n)
+print(t)
+df=pd.DataFrame(np.random.randn(n,10),index=t, columns=list('ABCDEFGHIJ'))
+print(df.describe())
